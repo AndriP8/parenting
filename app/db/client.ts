a5f0f960efc229f87f78708fd04addf48ad8dc75
Andri Purnomo <andri.adrp@gmail.com>
2026-08-06 17:23:10 +0700

    fix: correct docker-compose pg config and db client env loading

--- app/db/client.ts
+++ app/db/client.ts
@@ -1,11 +1,11 @@
+import 'dotenv/config'
+
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { migrate as drizzleMigrate } from 'drizzle-orm/node-postgres/migrator'
 import pg from 'pg'
 import * as schema from './schema'
 
-const connectionString =
-  process.env.DATABASE_URL ||
-  'postgres://postgres:postgres@127.0.0.1:5432/parenting_db'
+const connectionString = process.env.DATABASE_URL
 
 export const pool = new pg.Pool({
   connectionString,
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'drizzle-kit'
+import 'dotenv/config'
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: './app/db/schema.ts',
   out: './drizzle',
   dbCredentials: {
-    url:
-      process.env.DATABASE_URL ||
-      'postgres://postgres:postgres@127.0.0.1:5432/parenting_db',
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    url: process.env.DATABASE_URL!,
   },
 })
